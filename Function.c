8b5b231f74e6910f24016a0b029a0a30253aa747
Yasiru Lakshan <yasiru1999@gmail.com>
2021-08-10 21:48:34 +0530

    4th program

---
+++ Function.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d){
+    int ans = 0;
+    if ( a > ans){
+        ans = a;
+    } 
+
+    if ( b > ans){
+        ans = b;
+    }
+
+    if ( c > ans){
+        ans = c;
+    }
+
+    if ( d > ans){
+        ans = d;
+    }
+
+    return ans;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
