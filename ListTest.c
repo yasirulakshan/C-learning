efd58783dc6ea3a822102f472fee1e6e39992ac1
yasirulakshan <yasiru1999@gmail.com>
2021-08-15 20:15:52 +0530

    Still working

--- ListTest.c
+++ ListTest.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-void addNum(int* lst, int index){
-
+void addNum(int* lst, int newNum, int* length){
+    length++;
+    lst = realloc(lst,10 * sizeof(int));
+    lst[*length] = newNum;
 }
 
 
 int main(){
-    int* lst = (int *) malloc(sizeof(int));
-    lst = 10;
-
+    int length = 0;
+    int* lst = (int*)malloc(10*sizeof(int));
+    lst[0] = 10;
+    //addNum(lst,25,length);
+    //addNum(lst,323,length);
+    printf("%d\n",length);
+    //printf("%d\n",lst[1]);
+    //printf("%d\n",lst[2]);
+    return 0;
 }
