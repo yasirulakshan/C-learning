ae766a9490ca74544436fba0a8045f9eb217c03b
yasirulakshan <yasiru1999@gmail.com>
2021-08-16 20:16:26 +0530

    Found how to realloc :-)

--- ListTest.c
+++ ListTest.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 void addNum(int* lst, int newNum, int* length){
-    length++;
-    lst = realloc(lst,10 * sizeof(int));
+    *length =  *length+1;
+    lst = realloc(lst,*length * sizeof(int));
     lst[*length] = newNum;
 }
 
 
 int main(){
-    int* length = (int*)malloc(sizeof(int));
-    length[0] = 0;
-    int* lst = (int*)malloc(10*sizeof(int));
+    int* length = malloc(sizeof(int));
+    *length = 0;
+    int* lst = (int*)malloc(sizeof(int));
     lst[0] = 10;
-    //addNum(lst,25,length);
-    //addNum(lst,323,length);
-    printf("%d\n",*length);
-    //printf("%d\n",lst[1]);
-    //printf("%d\n",lst[2]);
+    addNum(lst,25,length);
+    addNum(lst,323,length);
+    printf("%d\n",lst[0]);
+    printf("%d\n",lst[1]);
+    printf("%d\n",lst[2]);
     return 0;
 }
